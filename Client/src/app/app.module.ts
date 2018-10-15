d5d30e117b6ee94f2a9840a0956dcbae32078173
Alejandro Peral Taboada <alejandropta@ext.inditex.com>
2018-10-15 09:17:21 +0200

    clientInit

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./store/reducers";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
+import { EffectsModule } from "@ngrx/effects";
+import { AppEffects } from "./store/effects/app.effects";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
