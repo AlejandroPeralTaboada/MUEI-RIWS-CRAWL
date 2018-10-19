3a57c3338a8565244a2d24bc4619de384da394ab
aida <waleaida@gmail.com>
2018-10-19 19:47:55 +0200

    Config elastic and scrapy in the same docker network

--- Elasticsearch/run.sh
+++ Elasticsearch/run.sh
@@ -1,1 +1,2 @@
-docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v $(pwd)/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml docker.elastic.co/elasticsearch/elasticsearch:6.4.2 
+doker network create net || true
+docker run -d --name elastic --net net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v $(pwd)/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml docker.elastic.co/elasticsearch/elasticsearch:6.4.2 
