8fb8377dfb8e581e39d7d5b346f565c9a54ea840
Alejandro Peral <alejandroperaltaboada@gmail.com>
2018-10-23 20:05:21 +0200

    navigation to details

--- Client/src/app/views/home/game-card/game-card.component.ts
+++ Client/src/app/views/home/game-card/game-card.component.ts
@@ -4,16 +4,16 @@ import { GiftCard } from '../../../store/model/Gift';
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
-  styleUrls: ['./game-card.component.css']
+  styleUrls: ['./game-card.component.scss']
 })
 export class GameCardComponent implements OnInit {
 
-  @Input() game:GiftCard
-  photo:string
+  @Input() game: GiftCard
+  photo: string
   constructor() { }
 
   ngOnInit() {
-    this.photo = `https://steamcdn-a.akamaihd.net/steam/apps/${this.game.idGame}/header_292x136.jpg` 
-  } 
+    this.photo = `https://steamcdn-a.akamaihd.net/steam/apps/${this.game.idGame}/header_292x136.jpg`
+  }
 
 }
